44af35fb87aa82f63f122ea8bb83bf68a8ac0de3
AntonioMarcosCorreaPinto <marcoscinconav@hotmail.com>
2019-03-09 13:32:21 -0300

    Lesson one hundred fiftenn teacher Nelio Alves

---
+++ LessonOneHundredFifteen/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace LessonOneHundredFifteen.Entities.Enums
+{
+    enum OrderStatus : int
+    {
+        PendingPayment = 0,
+        processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
---
+++ LessonOneHundredFifteen/Program.cs
@@ -0,0 +1,37 @@
+﻿using LessonOneHundredFifteen.Entities;
+using LessonOneHundredFifteen.Entities.Enums;
+using System;
+
+namespace LessonOneHundredFifteen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+
+            };
+
+
+            Console.WriteLine(order);
+
+            // It coverring type OrderStatus to string
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            Console.WriteLine(txt);
+
+            // It covering type string to OrderStatus
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered"); 
+            // It have the same value that exist at the enum "OrderStatus"
+
+            Console.WriteLine(os);
+
+
+        }
+    }
+}
